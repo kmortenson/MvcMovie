48e18caab0e812b50693b25c14208d45ed00f073
Kirk Mortenson <46416608+kmortenson@users.noreply.github.com>
2020-03-06 22:37:21 -0800

    basic finished

---
+++ Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "17 Miracles",
+                        ReleaseDate = DateTime.Parse("2011-1-1"),
+                        Genre = "History",
+                        Rating = "NR",
+                        Price = 14.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ephraim's Rescue",
+                        ReleaseDate = DateTime.Parse("2013-1-1"),
+                        Genre = "Adventure",
+                        Rating = "NR",
+                        Price = 14.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ultimate Questions",
+                        ReleaseDate = DateTime.Parse("2014-1-1"),
+                        Genre = "Documentary",
+                        Rating = "NR",
+                        Price = 19.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Book of Mormon and the New World DNA",
+                        ReleaseDate = DateTime.Parse("2008-9-30"),
+                        Genre = "Documentary",
+                        Rating = "NR",
+                        Price = 12.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
